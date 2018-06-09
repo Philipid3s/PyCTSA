16cb5e4ae95131b835bd13649e6a3590a381d7f6
Julien <julien.regnault@gmail.com>
2018-06-09 18:22:30 +0800

    Update settings.py

--- WebsiteCTSA/settings.py
+++ WebsiteCTSA/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hqp-w2ohh7jl3))^d%dftehq_bdb4&t&fd!n10=jsf^2t!wwv2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
